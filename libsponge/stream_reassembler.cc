84de2c8a60c6ff8ba7ea91844dcb169bcd1372ed
1658569545 <1658569545@qq.com>
2025-02-19 15:56:43 +0800

    lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,11 +1,10 @@
 #include "stream_reassembler.hh"
 
-// Dummy implementation of a stream reassembler.
+//流重组器的虚拟实现。
 
-// For Lab 1, please replace with a real implementation that passes the
-// automated checks run by `make check_lab1`.
+//对于Lab 1，请替换为通过‘ make check_lab1 ’运行的自动检查的真实实现。
 
-// You will need to add private members to the class declaration in `stream_reassembler.hh`
+//你需要在‘ stream_reassembly .hh ’中的类声明中添加私有成员
 
 template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
@@ -14,13 +13,15 @@ using namespace std;
 
 StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
 
-//! \details This function accepts a substring (aka a segment) of bytes,
-//! possibly out-of-order, from the logical stream, and assembles any newly
-//! contiguous substrings and writes them into the output stream in order.
+
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
 
-bool StreamReassembler::empty() const { return {}; }
+}
+
+bool StreamReassembler::empty() const {
+
+}
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -5,48 +5,53 @@
 
 #include <cstdint>
 #include <string>
-
-//! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
-//! possibly overlapping) into an in-order byte stream.
+#include <bits/stdc++.h>
+// 一个类，它将字节流中的一系列摘录（可能是无序的，也可能是重叠的）组装成有序的字节流。
 class StreamReassembler {
   private:
-    // Your code here -- add private members as necessary.
 
-    ByteStream _output;  //!< The reassembled in-order byte stream
-    size_t _capacity;    //!< The maximum number of bytes
+    // 按顺序重新组装的字节流
+    ByteStream _output; 
+
+    // 最大字节数
+    size_t _capacity;   
 
   public:
-    //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
-    //! \note This capacity limits both the bytes that have been reassembled,
-    //! and those that have not yet been reassembled.
+
+    /**
+     * @brief 构造函数
+     * @param[in] capacity 流重组器的容积
+     */
     StreamReassembler(const size_t capacity);
 
-    //! \brief Receives a substring and writes any newly contiguous bytes into the stream.
-    //!
-    //! If accepting all the data would overflow the `capacity` of this
-    //! `StreamReassembler`, then only the part of the data that fits will be
-    //! accepted. If the substring is only partially accepted, then the `eof`
-    //! will be disregarded.
-    //!
-    //! \param data the string being added
-    //! \param index the index of the first byte in `data`
-    //! \param eof whether or not this segment ends with the end of the stream
+    /**
+     * @brief 接收一个子字符串并将任何新的连续字节写入流。
+     * @param[in] data 正在添加的字符串
+     * @param[in] index data 中第一个字节的索引
+     * @param[in] eof 该段是否以流的结尾结束
+     * @attention 此函数从逻辑流中接受一个子字符串（也称为字节段），可能是无序的，需要组装任何新的连续子字符串并按顺序将它们写入输出流。
+     */
     void push_substring(const std::string &data, const uint64_t index, const bool eof);
 
-    //! \name Access the reassembled byte stream
-    //!@{
+    /**
+     * @brief 访问重新组装的字节流
+     */
     const ByteStream &stream_out() const { return _output; }
+    /**
+     * @brief 访问重新组装的字节流
+     */
     ByteStream &stream_out() { return _output; }
-    //!@}
-
-    //! The number of bytes in the substrings stored but not yet reassembled
-    //!
-    //! \note If the byte at a particular index has been submitted twice, it
-    //! should only be counted once for the purpose of this function.
+  
+    /**
+     * @brief 子字符串中已存储但尚未重新组装的字节数，即已经进入流重组器，但是还没有被写入流缓冲区的
+     * @attention 如果特定索引处的字节被提交了两次，则在此函数中应该只计算一次。
+     */
     size_t unassembled_bytes() const;
 
-    //! \brief Is the internal state empty (other than the output stream)?
-    //! \returns `true` if no substrings are waiting to be assembled
+    /**
+     * @brief 内部状态是空的（除了输出流）吗？
+     * @return 如果没有子字符串等待组装，则返回true
+     */
     bool empty() const;
 };
 
